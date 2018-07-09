1d2c57f00e8e3aa29658551f9ddd35e9501b6538
lucerohospina <lucerojhp@gmail.com>
2018-07-09 12:58:20 -0500

    uploading complex routing example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,72 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { CoursesComponent } from './courses/courses.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { CourseCardsComponent } from './courses/course-cards/course-cards.component';
+import { SideMenuComponent } from './side-menu/side-menu.component';
+import { CoursesCategoryComponent } from './courses/courses-category/courses-category.component';
+
+const routerConfig: Routes = [
+  {
+      path: 'home',
+      component: HomeComponent
+  },
+  {
+      path: 'about',
+      component: AboutComponent
+  },
+  {
+      path: 'courses',
+      component: CoursesComponent,
+      children: [
+      {
+        path: '',
+        component: CourseCardsComponent
+      },
+      {
+        path: ':id',
+        component: CoursesCategoryComponent
+      },
+      {
+        path: '',
+        outlet: 'sidemenu',
+        component: SideMenuComponent
+      },
+      {
+        path: ':id',
+        outlet: 'sidemenu',
+        component: SideMenuComponent
+      }
+    ]
+  },
+  {
+      path: '',
+      redirectTo: '/home',
+      pathMatch: 'full'
+  },
+  {
+      path: '**',
+      redirectTo: '/home',
+      pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesComponent,
+    AboutComponent,
+    HomeComponent,
+    CourseCardsComponent,
+    SideMenuComponent,
+    CoursesCategoryComponent,
   ],
   imports: [
+    RouterModule.forRoot(routerConfig),
     BrowserModule
   ],
   providers: [],
